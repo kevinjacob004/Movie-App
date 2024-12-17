8aa0b7fa10033ca9e49c45aab0123700ca5325eb
kevinjacob004 <kevinjacobmathew.004@gmail.com>
2024-12-17 11:58:47 +0530

    logo added

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.movieapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String getUname,getPass;
+    EditText e1,e2;
+    Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        e1=(EditText) findViewById(R.id.uname);
+        e2=(EditText) findViewById(R.id.pass);
+        b1=(Button) findViewById(R.id.but1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUname=e1.getText().toString();
+                getPass=e2.getText().toString();
+            }
+        });
 
     }
 }
